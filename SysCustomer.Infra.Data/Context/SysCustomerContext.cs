3dfe2d3d137d53a08a8cd47d8bd6b11fbb22ccd5
Guilherme <g,.s.padilha@hotmail.com>
2019-02-28 15:06:34 -0300

    Construtor DbContext

--- SysCustomer.Infra.Data/Context/SysCustomerContext.cs
+++ SysCustomer.Infra.Data/Context/SysCustomerContext.cs
@@ -6,10 +6,12 @@ namespace SysCustomer.Infra.Data.Context
     public class SysCustomerContext : DbContext
     {
 
-        public SysCustomerContext(DbContextOptions<SysCustomerContext> options)
-            : base(options){
-        }
+        public SysCustomerContext()
+            : base(new DbContextOptions<SysCustomerContext>())
+    {
+      //.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"])
+    }
 
-        public DbSet<Customer> Customers { get; set; }
+    public DbSet<Customer> Customers { get; set; }
     }
 }
