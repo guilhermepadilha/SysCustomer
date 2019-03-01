c9e1b961b2ccc6d93491a8fa0b8decf2054cd78c
Guilherme Padilha <g.s.padilha@hotmail.com>
2019-03-01 01:00:33 -0300

    controller

---
+++ SysCustomer.WebApi/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace SysCustomer.WebApi.Controllers
+{
+    [Route("api/syscustomer")]
+    public class CustomerController : Controller
+    {
+        [HttpGet]
+        [Route("customer/{customerId:int}")]
+        public HttpResponseMessage GetCustomer (int customerId)
+        {
+            try
+            {
+                var _gamePointRepository = new CustomerRepository();
+
+
+                //return Request.CreateResponse(HttpStatusCode.OK, _gamePointRepository.GetAllPlayers(gameId));
+
+            }
+            catch (System.Exception ex)
+            {
+                //return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+    }
+}
