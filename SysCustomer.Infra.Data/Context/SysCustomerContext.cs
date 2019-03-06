b743a140e3bcb218c0f5bdf2cc81019a73780caa
Guilherme <g,.s.padilha@hotmail.com>
2019-03-06 15:39:20 -0300

    Correção de defeito na classe SysCustomerContext

--- SysCustomer.Infra.Data/Context/SysCustomerContext.cs
+++ SysCustomer.Infra.Data/Context/SysCustomerContext.cs
@@ -1,25 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿
+
+using Microsoft.EntityFrameworkCore;
 using SysCustomer.Domain.Entities;
 
 namespace SysCustomer.Infra.Data.Context
 {
-    public class SysCustomerContext : DbContext
+  public class SysCustomerContext : DbContext
+  {
+    public DbSet<Customer> Customers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        public DbSet<Customer> Customers { get; set; }
+        optionsBuilder.UseSqlServer(@"Data Source = (localdb)\\mssqllocaldb; Initial Catalog = DataBase; Integrated Security = True");
     }
 
-    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    //{
-    //    optionsBuilder.UseSqlServer(@"Data Source = (localdb)\\mssqllocaldb; Initial Catalog = DataBase; Integrated Security = True");
+  }
 
-    //    //if (!optionsBuilder.IsConfigured)
-    //    //{
-    //    //    IConfigurationRoot configuration = new ConfigurationBuilder()
-    //    //       .SetBasePath(Directory.GetCurrentDirectory())
-    //    //       .AddJsonFile("appsettings.json")
-    //    //       .Build();
-    //    //    var connectionString = configuration.GetConnectionString("DbCoreConnectionString");
-    //    //    optionsBuilder.UseSqlServer(connectionString);
-    //    //}
-    //}
+
 }
