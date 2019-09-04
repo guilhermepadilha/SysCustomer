97dcdd00b40fd2b0c3b77c9e579a05601670e2ac
Guilherme Padilha <g.s.padilha@hotmail.com>
2019-09-04 00:43:47 -0300

    projeto de exemplo jwt auth angular

--- client/sys-customer/src/app/shared/customer-detail.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CustomerDetail } from './customer-detail.model';
-
-describe('CustomerDetail', () => {
-  it('should create an instance', () => {
-    expect(new CustomerDetail()).toBeTruthy();
-  });
-});
